16478e5b51b2987e66fc919f186fcb3676e4a047
nithish-27 <rajnithish27@gmail.com>
2023-10-27 17:38:30 +0530

    latest

--- routes/backend/page.js
+++ routes/backend/page.js
@@ -3,7 +3,7 @@ const app = express();
 const router = express();
 const PORT = 200;
 const pageModel = require("../../model/pageModel");
-
+const axios = require("axios");
 //Routes
 router.get("/jwell", (req, res) => {
   res.render("../views/Jwell/jwell");
@@ -21,14 +21,41 @@ router.get("/banners", (req, res) => {
 router.get("/newsfeed", (req, res) => {
   res.render("../views/Newsfeed/newsfeed");
 });
-router.get("/details", (req, res) => {
-  res.render("../views/Jwell/Jwell-detail");
+router.get("/details", async (req, res) => {
+  try {
+    const response = await axios(
+      "https://yts-restapi.onrender.com/purple/v1/jewel/"
+    );
+    const jewelryData = response.data.Jewellery_details;
+    console.log(jewelryData);
+    res.render("../views/Jwell/Jwell-detail", { jewelryData });
+  } catch (error) {
+    console.log(error);
+  }
 });
-router.get("/bannerdetails", (req, res) => {
-  res.render("../views/Banner/banners-detail");
+router.get("/bannerdetails", async (req, res) => {
+  try {
+    const response = await axios(
+      "https://yts-restapi.onrender.com/purple/v1/banner/"
+    );
+    const bannerData = response.data.Banner_details;
+    console.log(bannerData);
+    res.render("../views/Banner/banners-detail", { bannerData });
+  } catch (error) {
+    console.log(error);
+  }
 });
-router.get("/newsdetails", (req, res) => {
-  res.render("../views/Newsfeed/Newsfeed-detail");
+router.get("/newsdetails", async (req, res) => {
+  try {
+    const response = await axios(
+      "https://yts-restapi.onrender.com/purple/v1/newsfeed/"
+    );
+    const newsfeedData = response.data.newsfeed_details;
+    console.log(newsfeedData);
+    res.render("../views/Newsfeed/Newsfeed-detail", { newsfeedData });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
