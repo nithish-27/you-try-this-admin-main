40ba1a009637e32b06515d494b045a0c2177b788
nithish-27 <rajnithish27@gmail.com>
2023-11-07 20:13:55 +0530

    dashboard

--- routes/backend/admin.js
+++ routes/backend/admin.js
@@ -1,12 +1,44 @@
-let express = require('express');
-let app = express();
+let express = require("express");
 let router = express();
-let PORT = 200;
-
+const axios = require("axios");
 //Routes
+async function fetchCountsFromExternalAPI() {
+  try {
+    const jewels = await axios.get(
+      "https://yts-restapi.onrender.com/purple/v1/jewel/"
+    );
 
-router.get('/',(req,res)=>{
-    res.render('../views/index')
-})
+    const newsfeeds = await axios.get(
+      "https://yts-restapi.onrender.com/purple/v1/newsfeed/"
+    );
+    const banners = await axios.get(
+      "https://yts-restapi.onrender.com/purple/v1/banner/"
+    );
+    const users = await axios.get(
+      "https://yts-restapi.onrender.com/purple/v1/user/get/"
+    );
+    const counts = {
+      jewels: jewels.data.Jewellery_details.length,
+      newsfeeds: newsfeeds.data.newsfeed_details.length,
+      banners: banners.data.Banner_details.length,
+      users: users.data.userCount,
+    };
+    console.log(counts);
+    return counts;
+  } catch (error) {
+    console.error(error);
+    return {
+      jewels: "Loading...",
+      newsfeeds: "Loading...",
+      banners: "Loading...",
+      // users: users.length,
+    };
+  }
+}
+router.get("/", async (req, res) => {
+  const counts = await fetchCountsFromExternalAPI();
 
-module.exports = router
+  res.render("../views/index", { counts });
+});
+
+module.exports = router;
